91bbec6486c329d816bddf3c5446e2e6201f25e1
Pankaj4152 <pankajgoyal4152@gmail.com>
2025-11-24 01:31:02 +0530

    basic crud

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
+from random import randrange
+
+app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1},
+            {"title": "title of post 2", "content": "content of post 2", "id": 2}]
+
+@app.get("/")
+def root():
+    return {"message": "Welome to my API"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": my_posts}
+ 
+@app.post("/posts")
+def create_post(post: Post):
+    post_dict = post.model_dump()   
+    post_dict['id'] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"post": post_dict}
